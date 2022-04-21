edbbc6a395c2d8a88073671626739ec22f837505
exerussus <solovyov.production@gmail.com>
2022-04-21 12:27:07 +0300

    Поправлен баг с точкой, введена защита от SyntaxError.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
-
+from random import choice
 
 Config.set('graphics', 'resizable', 0)
 Config.write()
@@ -19,15 +19,26 @@ Config.write()
 class CalculatorApp(App):
 
     def add_point(self, instance):
-        if self.form_text == '0':
-            self.form_text == '0.'
-            print(self.form_text)
-            self.update_label()
-        else:
-            self.form_text += instance.text
-            self.update_label()
+        key = 1
+        for i in self.form_text:
+            if i == '*' or i == '+' or i == '/' or i == '-':
+                key += 1
+            if i == '.':
+                key -= 1
+        if key >= 1:
+            if self.form_text == '':
+                self.form_text == str('0' + instance.text)
+                print(self.form_text)
+                self.lbl.text = '0.'
+            else:
+                self.form_text += instance.text
+                self.update_label()
 
-
+    def wat(self):
+        wat_list = ['Шутите?', "Попробуйте ещё раз.", "У Вас почти получилось! Почти...", "Может, ещё одна попытка?",
+                    'Что-то пошло не так...', "Боюсь, где-то допущена ошибка."
+        ]
+        return choice(wat_list)
 
     def clearing(self, instance):
         if instance.text == '<--':
@@ -44,27 +55,29 @@ class CalculatorApp(App):
 
     def add_number(self, instance):
         if self.form_text == '0':
-            self.form_text = ''
-        self.form_text += str(instance.text)
+            self.form_text = '' + str(instance.text)
+        elif self.form_text == '0.':
+            self.form_text = '0.' + str(instance.text)
+        else:
+            self.form_text += str(instance.text)
         self.update_label()
 
-
     def add_operation(self, instance):
-        # self.formula = int(self.form_text)
         self.form_text += ' ' + str(instance.text) + ' '
         self.update_label()
 
-
-
     def calc(self, instance):
-        answer = eval(self.form_text)
-        self.form_text = self.form_text + '=' + str(answer)
-        self.update_label()
-        self.form_text = str(answer)
-
+        try:
+            answer = eval(self.form_text)
+            self.form_text = self.form_text + '=' + str(answer)
+            self.update_label()
+            self.form_text = str(answer)
+        except SyntaxError:
+            self.form_text = '0'
+            self.lbl.text = self.wat()
 
     def build(self):
-        self.form_text = ''
+        self.form_text = '0'
         bl = BoxLayout(orientation='vertical', padding=5)
         gl = GridLayout(cols=4, spacing=1, size_hint=(1, .6))
         self.lbl = Label(text='0', font_size=40, halign='right', valign='bottom', size_hint=(1, .4), text_size=(400 - 80, 500 * .4 - 50))
